a07102f2921a3e3a681f147d23dca98b28e19b87
Azriel fauzi hermansyah <55310608+Azrielfhr2@users.noreply.github.com>
2020-11-11 21:34:12 +0700

    Add files via upload

---
+++ Program Loop/src/com/Project/Main.java
@@ -0,0 +1,24 @@
+package com.Project;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int jum = 100;
+
+        for (int i = 0;i <= jum ; i++){
+
+            System.out.println("Saya luar biasa");
+
+            if(i % 10 == 0 ){
+                System.out.println("Saya Hebat");
+            }
+
+            if (i % 20 == 0){
+                System.out.println("Saya bisa");
+            }
+
+        }
+
+    }
+}
